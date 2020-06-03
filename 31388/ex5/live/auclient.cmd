b889fd15b5916c2083da8a6a4051b1376695ae82
manxilin <matthewlin98@hotmail.com>
2020-06-03 11:52:28 +0200

    update AAS

---
+++ 31388/ex5/live/auclient.cmd
@@ -0,0 +1,2 @@
+cam connect=smr15:24920
+cam poolget img=18 fmt="rgb" all
---
+++ 31388/ex5/live/qclient.cmd
@@ -0,0 +1,35 @@
+cam connect=smr8:24920
+cam kinect open
+cam poolget img=18 all
+cam gmkget
+cam connect=smr15:24920
+cam poolget img=18 fmt="rgb" all
+kinect open
+cam kinect open
+cam poolget img=18 fmt="rgb" all
+cam connect=smr15:24920
+cam poolget img=18 fmt="rgb" all
+cam connect=smr15:24920
+cam poologet img=18 all
+cam poolget img=18 all
+cam poolget img=18 fmt="rgb" all
+cam connect=smr15:24920
+cam poolget img=18 fmt="rgb" all
+poolget help
+cam poolget img=18 fmt=rgb all
+cam poolget img=18 fmt=RGB all
+cam connect=smr15:24920
+cam poolget img=18 fmt=rgb all
+cam poolget img=18  all
+cam poolget img=18  fmt=rgb all
+cam gmkget
+cam poolget img=18  fmt=rgb all
+cam gmkget
+cam connect= smr12:24920
+cam connect=smr6:24920
+cam kinect open
+cam poolget img=18 fmt=rgb all
+cam connect = smr6:24920
+cam kinect open
+cam poolget img=18 fmt=rgb all
+cam poolget img=18 savePNG="focuslength.png"
